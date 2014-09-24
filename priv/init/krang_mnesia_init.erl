a108c8d3d2ce199cb6866f8320a53d594c091f15
ruanpienaar <ruan800@gmail.com>
2014-09-25 00:44:08 +0100

    db mnesia init

---
+++ priv/init/krang_mnesia_init.erl
@@ -0,0 +1,39 @@
+-module(krang_mnesia_init).
+-export([init/0, stop/1]).
+
+-define(APPNAME,krang).
+
+init() ->
+    init_db ().
+
+init_db () ->
+  init_db ([node ()]).
+
+init_db (Nodes) ->
+  mnesia:create_schema (Nodes),
+  mnesia:change_table_copy_type (schema, node(), disc_copies), 
+  mnesia:start (),
+  ModelList = [ list_to_atom (M) || M <- boss_files:model_list (?APPNAME) ],
+  ExistingTables = mnesia:system_info(tables),
+  Tables = (ModelList ++ ['_ids_']) -- ExistingTables,
+  create_model_tables (Nodes, Tables).
+
+create_model_tables (_, []) -> ok;
+create_model_tables (Nodes, [Model | Models]) ->
+  [create_model_table (Nodes, Model)] ++
+   create_model_tables (Nodes, Models).
+
+create_model_table (Nodes, '_ids_') ->
+  create_table (Nodes, '_ids_', [type, id]);
+
+create_model_table (Nodes, Model) ->
+  Record = boss_record_lib:dummy_record (Model),
+  { Model, create_table (Nodes, Model, Record:attribute_names ()) }.
+
+create_table (Nodes, Table, Attribs) ->
+  mnesia:create_table (Table,
+    [ { disc_copies, Nodes   },
+      { attributes,  Attribs } ]).
+
+stop(_) ->
+    ok.
