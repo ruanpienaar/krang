6dcee6863e8a0db942b5a1f4849c4cea386ca549
ruanpienaar <ruan800@gmail.com>
2014-11-14 20:34:45 +0000

    ..

--- src/controller/krang_auth_controller.erl
+++ src/controller/krang_auth_controller.erl
@@ -8,16 +8,20 @@ login('POST',URI) ->
     {output,<<"ok">>}.
 
 signup('POST',URI) ->
-    case lists:map(fun(PostParam) -> Req:post_param(PostParam) end, ["r_em","r_pw","r_cpw"]) of
+    case lists:map(fun(PostParam) -> Req:post_param(PostParam) end,
+            ["r_em","r_pw","r_cpw"]) of
         [Email,Pwd,CPwd] when Email =:= undefined;
                               Pwd   =:= undefined;
                               CPwd  =:= undefined ->
+            krang_common:log("All Input fields empty!"),
             {redirect,"auth"}; %% XXX Improve errors !!!
         [Email,Pwd,CPwd] when Pwd =/= CPwd ->
+            krang_common:log("Password Confirmation doesn't match!"),
             {redirect,"auth"}; %% XXX Improve errors !!!
         [Email,Pwd,CPwd] when Pwd =:= CPwd ->
+            krang_common:log("Everything good!"),
             %% generate verify token
-            VerifyToken = "test",
+            VerifyToken = "a1b2c3d4",
             %% Create email
             %% Send email here
             %% Hash Pwd
@@ -31,4 +35,5 @@ signup('POST',URI) ->
             end
     end.
 
-success()
+success() ->
+    ok.
